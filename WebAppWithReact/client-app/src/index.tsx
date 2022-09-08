07f485cad09b1e01852181c8c0721976a3c43441
Roman <Khlebnikov.Roman187@gmail.com>
2022-09-08 19:34:04 +0300

    Added map libraries to project, and index page now show map

--- WebAppWithReact/client-app/src/index.tsx
+++ WebAppWithReact/client-app/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Map1 from "./map1";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Map1 />
   </React.StrictMode>
 );
 
---
+++ WebAppWithReact/client-app/src/map1.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {MapContainer, TileLayer} from "react-leaflet";
+import {latLng} from "leaflet";
+import './Map.css';
+
+class MapComponent extends React.Component{
+    state = {
+        lat: 59.918711823015684,
+        lng: 30.319212156536604,
+        zoom: 120
+    }
+    render() {
+        const center = [this.state.lat, this.state.lng];
+        //const center2 = new LatLngTuple(center)
+        // @ts-ignore
+        return (
+            <MapContainer zoom={this.state.zoom} center={latLng(this.state.lat, this.state.lng)}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        );
+    }
+}
+
+export default MapComponent;
