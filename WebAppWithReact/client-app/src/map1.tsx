1048afae6aa99693b6c8513201ce721a3af2c52c
Roman <Khlebnikov.Roman187@gmail.com>
2022-09-16 12:15:11 +0300

    nothing interesting

--- WebAppWithReact/client-app/src/map1.tsx
+++ WebAppWithReact/client-app/src/map1.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import {MapContainer, TileLayer} from "react-leaflet";
 import {latLng} from "leaflet";
 import './Map.css';
-
+import { DownloadOutlined } from '@ant-design/icons';
+import { Button, Radio } from 'antd';
+import type { SizeType } from 'antd/es/config-provider/SizeContext';
+import { useState } from 'react';
+import 'antd/dist/antd.css'
 class MapComponent extends React.Component{
+    
     state = {
         lat: 59.918711823015684,
         lng: 30.319212156536604,
@@ -14,12 +19,20 @@ class MapComponent extends React.Component{
         //const center2 = new LatLngTuple(center)
         // @ts-ignore
         return (
-            <MapContainer zoom={this.state.zoom} center={latLng(this.state.lat, this.state.lng)} attributionControl={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
+            <div >
+                
+                <MapContainer  zoom={this.state.zoom} center={latLng(this.state.lat, this.state.lng)} attributionControl={false} style = {{zIndex:1, position:"relative", top:0,left:0}}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                </MapContainer>
+                <div style = {{zIndex:999, position:"relative", top:0,left:0}}>
+                    <Button type="primary" shape={"round"} >Primary</Button>
+                    <Button shape={"round"}>Default</Button>
+                    <Button type="dashed" shape={"round"}>Dashed</Button>
+                </div>
+            </div>
         );
     }
 }
