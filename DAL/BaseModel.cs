11a05b0765a1702623f8840bf9126e9cd42df3db
Roman <Khlebnikov.Roman187@gmail.com>
2022-10-12 18:21:47 +0300

    added DAL

---
+++ DAL/BaseModel.cs
@@ -0,0 +1,7 @@
+namespace Models
+{
+    //public abstract class BaseModel
+    //{
+    //    public Guid Id { get; set; }
+    //}
+}
---
+++ DAL/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
