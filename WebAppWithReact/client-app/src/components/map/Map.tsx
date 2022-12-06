ca2e3271078725bbfcae3947c9284b17cd728479
Roman <Khlebnikov.Roman187@gmail.com>
2022-12-06 20:22:59 +0300

    добавлен хитмэп с тестовыми данными

--- WebAppWithReact/client-app/src/components/map/Map.tsx
+++ WebAppWithReact/client-app/src/components/map/Map.tsx
@@ -1,8 +1,6 @@
 import React, {MouseEvent, useCallback, useMemo, useState} from "react";
 import {MapContainer, Marker, Popup, TileLayer, ZoomControl} from "react-leaflet";
-import {DomEvent, latLng} from "leaflet";
-import L from 'leaflet';
-import SelectLayers from 'leaflet';
+import L, {latLng} from "leaflet";
 import './Map.css';
 import {Button, Layout} from 'antd';
 import 'antd/dist/antd.css'
@@ -10,21 +8,24 @@ import {Content} from "antd/es/layout/layout";
 import MenuSider from "../menu/menuSider/MenuSider";
 import {useContextMenu} from "../../hooks";
 import MenuLayers from "../menu/menuLayers/MenuLayers";
+import {HeatmapLayer} from "react-leaflet-heatmap-layer-v3/lib";
+import {addressPoints} from "./exampleData";
 
 L.Icon.Default.imagePath = "https://unpkg.com/browse/leaflet@1.9.2/dist/images/";
 
 export const MapComponent: React.FC = () => {
     const [lat, setLat] = useState(59.918711823015684);
     const [lng, setlng] = useState(30.319212156536604);
-    
-    const { setContextMenu } = useContextMenu();
-    
+
+    const {setContextMenu} = useContextMenu();
+
     const center = [lat, lng];
 
     const contextMenu = useMemo(() => [
         {
-            name: 'Option #1', 
-            onClick: () => {}
+            name: 'Option #1',
+            onClick: () => {
+            }
         },
         {
             name: 'Option #2',
@@ -32,18 +33,22 @@ export const MapComponent: React.FC = () => {
         },
         {
             name: 'Option #3',
-            onClick: () => {}
+            onClick: () => {
+            }
         },
     ], [])
-    
+
     const handleContextMenu = useCallback((event: MouseEvent) => {
         event.preventDefault();
         const {clientX, clientY} = event;
         console.log(clientX, clientY);
         setContextMenu(contextMenu, [clientX, clientY]);
-    },[setContextMenu, contextMenu])
-    
+    }, [setContextMenu, contextMenu])
+
     //const center2 = new LatLngTuple(center)
+    // @ts-ignore
+    // @ts-ignore
+    // @ts-ignore
     // @ts-ignore
     return (
         <div>
@@ -58,13 +63,23 @@ export const MapComponent: React.FC = () => {
                         <div onContextMenu={handleContextMenu}>
                             <MapContainer zoomControl={false} zoom={100} center={latLng(lat, lng)} className="big-map"
                                           attributionControl={false}
-                                          style={{ zIndex: 1, position: "relative", top: 0, left: 0 }}>
-                                <ZoomControl position={'bottomright'} />
+                                          style={{zIndex: 1, position: "relative", top: 0, left: 0}}>
+                                <ZoomControl position={'bottomright'}/>
+                                <HeatmapLayer
+                                    fitBoundsOnLoad
+                                    fitBoundsOnUpdate
+                                    points={addressPoints}
+                                    // @ts-ignore
+                                    longitudeExtractor={m => m[1]}
+                                    // @ts-ignore
+                                    latitudeExtractor={m => m[0]}
+                                    // @ts-ignore
+                                    intensityExtractor={m => parseFloat(m[2])}/>
                                 <TileLayer
                                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                 />
-                                <Marker position = {latLng(lat, lng)}>
+                                <Marker position={latLng(lat, lng)}>
                                     <Popup>
                                         Зачем сюда жмешь Э?
                                     </Popup>
