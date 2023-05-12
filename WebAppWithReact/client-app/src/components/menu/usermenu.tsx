5a39b92091599ca55d4c03c993505eaaaa89c467
Roman <Khlebnikov.Roman187@gmail.com>
2023-05-12 03:34:13 +0300

    рабочий логаут

--- WebAppWithReact/client-app/src/components/menu/usermenu.tsx
+++ WebAppWithReact/client-app/src/components/menu/usermenu.tsx
@@ -1,14 +1,23 @@
 import React, {useEffect, useState} from "react";
-import {Avatar, Card} from "antd";
+import {Avatar, Card, Modal} from "antd";
 import {LogoutOutlined, SettingOutlined, UserOutlined} from '@ant-design/icons';
 import UserService from "../../services/UserService";
 import Meta from "antd/es/card/Meta";
+import {useNavigate} from "react-router-dom";
 
 export const UserMenu: React.FC = () => {
     const [username, setusername] = useState("");
+    const [open, setOpen] = useState(false)
+    const navigate = useNavigate();
     useEffect(() => {
         setusername(UserService.getCurrentUserName()!)
     },);
+
+    function logout() {
+        UserService.logout();
+        navigate("/")
+    }
+
     return (
         <>
             {/*<Space.Compact block>*/}
@@ -18,10 +27,16 @@ export const UserMenu: React.FC = () => {
             {/*    </Button>*/}
             {/*    <Button type="primary">Button 1</Button>*/}
             {/*</Space.Compact>*/}
+            <Modal open={open}
+                   onOk={logout}
+                   onCancel={() => setOpen(false)}
+                   title="Вы действительно хотите выйти?">
+
+            </Modal>
             <Card hoverable style={{width: 300, marginTop: 16}}
                   actions={[
                       <SettingOutlined key="Настройки"/>,
-                      <LogoutOutlined key="Выход"/>,
+                      <LogoutOutlined key="Выход" onClick={() => setOpen(true)}/>,
                   ]}>
                 <Meta
                     avatar={<Avatar style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}/>}
