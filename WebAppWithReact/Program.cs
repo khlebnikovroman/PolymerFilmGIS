bc9b99705610de1eb7863a107fc7b5131933ccab
Roman <Khlebnikov.Roman187@gmail.com>
2023-06-14 00:07:52 +0300

    вроде как автосоздание и применение миграций

--- WebAppWithReact/Program.cs
+++ WebAppWithReact/Program.cs
@@ -141,16 +141,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//if (app.Environment.IsProduction())
-//{
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<Context>();
-    dbContext.Database.EnsureCreated();
-}
-
-//}
-
 // Configure the HTTP request pipeline.
 
 //app.UseHttpsRedirection();
