0e402460f4602c868c530d6869301a624df07bd2
Roman <Khlebnikov.Roman187@gmail.com>
2023-06-18 17:42:54 +0300

    обновил настроечки

--- WebAppWithReact/Features/User/DTO/GetUserSettingsDTO.cs
+++ WebAppWithReact/Features/User/DTO/GetUserSettingsDTO.cs
@@ -1,6 +1,6 @@
-﻿namespace WebAppWithReact.Features.User.DTO;
+namespace WebAppWithReact.Features.User.DTO;
 
-public class UpdateUserSettingsDTO
+public class GetUserSettingsDTO
 {
     public bool IsNeedToDrawHeatMap { get; set; }
     public double RadiusOfObjectWithMaxCapacityInKilometers { get; set; }
